98a7621f80c72f0019a9722a9648cd8450f5f0af
Tabrez Khan <khan.t.29@gmail.com>
2023-08-27 15:05:00 +0200

    update: add tags with icon

--- main/tags.lua
+++ main/tags.lua
@@ -4,15 +4,24 @@ local awful = require("awful")
 local _M = {}
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+local config_path = awful.util.getdir("config")
+
+local my_tags = {
+  { icon = config_path .. "/code.png" },
+  { icon = config_path .. "/chrome.png" },
+}
 
 function _M.get ()
   local tags = {}
 
   awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
-    tags[s] = awful.tag(
-      { "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, RC.layouts[1]
-    )
+    for i, t in ipairs(my_tags) do
+      local tag = awful.tag.add("", { icon = t.icon, screen = s, layout = RC.layouts[1], layouts = RC.layouts })
+      if i == 1 then
+        tag.selected = true
+      end
+    end
   end)
   
   return tags
